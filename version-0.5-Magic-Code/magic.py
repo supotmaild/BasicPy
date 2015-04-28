b924f98283dbcb0a02f462623e339c7bd2e0f921
Supot Sawangpiriyakij <therithwarich@gmail.com>
2015-04-28 18:06:00 +0700

    Update magic.py

--- version-0.5-Magic-Code/magic.py
+++ version-0.5-Magic-Code/magic.py
@@ -1,5 +1,5 @@
 '''How to use this code'''
 '''from console >>>'''
-'''from magic import magic'''
-'''run("C:\Pyton27\super5.py")'''
+'''import magic'''
+'''magic.run("C:\Pyton27\super5.py")'''
 def run(f):ff=open(f);exec(''.join(ff.readlines()))
